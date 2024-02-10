ee02d013efcc050d22fbb699189a2c67b9484a51
rrobinson421 <123380520+rrobinson421@users.noreply.github.com>
2024-02-10 09:35:03 -0500

    Initial commit

---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Queue extends JPanel {
+
+    /**
+     * Singular Panel
+     * Allows for the addition of "people" with specific modifiers
+     * It is a priority queue / "Mission Queue"
+     * Allows for the reading of csvs, or API database
+     */
+
+}
